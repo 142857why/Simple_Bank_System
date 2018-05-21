53f6f69037a4b56af2c84ce76f0523f0d88eb1a7
zhourui <zhourui_1997@126.com>
2018-05-21 23:34:07 +0800

    测试一下关于序列化(Serialization)的想法

---
+++ 想法测试/src/Person.java
@@ -0,0 +1,43 @@
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private Man man;
+    private String userName;
+    private transient int age;
+
+    public Man getMan() {
+        return man;
+    }
+
+    public void setMan(Man man) {
+        this.man = man;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person() {
+        System.out.println("Person constructed");
+    }
+
+    public Person(Man man, String userName, int age) {
+        this.man = man;
+        this.userName = userName;
+        this.age = age;
+    }
+
+
+}
